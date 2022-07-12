8b2d102722d5bab8245421a8e9e3b7c3c2ce72c9
yatochkaaa <volodymyr.taranin@gmail.com>
2022-07-12 19:49:07 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+import minimist from 'minimist';
+
+const greetings = () => {
+  const params = minimist(process.argv.slice(2));
+
+  return `${params.name} ${params.lastname} is ${params.age}`;
+}
+
+console.log(greetings());
